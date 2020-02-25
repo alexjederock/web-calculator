2dba34c962caa5d6b54ab2166ba2eadbf625c939
alexjederock <60864509+alexjederock@users.noreply.github.com>
2020-02-25 17:49:17 -0500

    Web Calculator Commit

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ function calc() {
     var field1 = document.getElementById("number1").value;
     var field2 = document.getElementById("number2").value;
    
-    var result = parseFloat(field1) + parseFloat(field2);
-
-    if(!isNaN(answer))
-    {
-        document.getElementById("result").innerHTML="The answer is "
-         +  result;
-    }
-
-
-
+    var result = parseInt(field1) + parseInt(field2);
+     document.getElementById('result').innerHTML = result;
 }
